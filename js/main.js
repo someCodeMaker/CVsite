09c5276a7869c7381228455bedb0111d615c8676
someCodeMaker <denishudzenko1@gmail.com>
2024-12-08 01:07:55 +0000

    add new modal pages and js logic for buttons

--- js/main.js
+++ js/main.js
@@ -1,30 +1,91 @@
-// menu button function
+// modal menu variables
 const menuButtonOpen = document.querySelector(".menu-button-open");
-const menuButtonClose = document.querySelector(".modal-menu__button--close");
+const menuButtonClose = document.querySelector(".modal-menu__button");
 const modalMenu = document.querySelector(".modal-menu");
 const menuLinks = document.querySelectorAll(".modal-menu__nav-list-item");
+const body = document.querySelector("body");
 
-menuButtonOpen.addEventListener("click", () => {
-  modalMenu.classList.remove("menu-closing");
-  modalMenu.classList.add("menu-open");
-});
+// modal form variables
+const modalForm = document.querySelector(".modal-form");
+const formButtonOpen = document.querySelectorAll(".form-open-button");
+const formButtonClose = document.querySelector(".modal-form__button--close");
+const thanksButtonClose = document.querySelector(".modal-form-thanks__button");
+const formSubmitButton = document.querySelector(".modal-form__submit-button");
+const formThankYou = document.querySelector(".modal-form-thanks");
 
-menuButtonClose.addEventListener("click", () => {
-  closeMenu();
-});
+// menu open/close button event
+buttonEvent(modalMenu, "open", "close", menuButtonOpen, menuButtonClose, menuLinks);
 
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    closeMenu();
+// modal form button close
+buttonEvent(modalForm, "open", "close", undefined, formButtonClose, undefined, formButtonOpen);
+//modal form submit button
+formSubmitButton.addEventListener("click", () => {
+  formThankYou.classList.add("form-open-thanks");
+  //modal form thanks button close
+  thanksButtonClose.addEventListener("click", () => {
+    modalForm.classList.remove("open");
+    formThankYou.classList.remove("form-open-thanks");
+    body.style.overflow = "visible";
   });
 });
 
-function closeMenu() {
-  modalMenu.classList.remove("menu-open");
-  modalMenu.classList.add("menu-closing");
+// add event to multiply elements
+function eventOpenArrElements(elements, modalWindow, openClass, closingClass) {
+  elements.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      body.style.overflow = "hidden";
+      modalWindow.classList.remove(closingClass);
+      modalWindow.classList.add(openClass);
+    });
+  });
+}
+
+function eventCloseArrElements(elem, modalWindow, openClass, closingClass) {
+  elem.forEach((link) => {
+    link.addEventListener("click", () => {
+      body.style.overflow = "visible";
+      closeModal(modalWindow, openClass, closingClass);
+    });
+  });
+}
+
+// closing button function
+function closeModal(modalWindow, openClass, closingClass) {
+  modalWindow.classList.remove(openClass);
+  modalWindow.classList.add(closingClass);
 
   // Wait for the transition duration (700ms)
   setTimeout(() => {
-    modalMenu.classList.remove("menu-closing");
+    modalWindow.classList.remove(closingClass);
   }, 500);
+}
+function openModal(modalWindow, openClass, closingClass) {
+  modalWindow.classList.remove(closingClass);
+  modalWindow.classList.add(openClass);
+}
+
+function buttonEvent(
+  modalWindow,
+  openClass,
+  closingClass,
+  openBtn,
+  closeBtn,
+  arrElemClose,
+  arrElemOpen
+) {
+  //open button
+  if (openBtn) {
+    openBtn.addEventListener("click", () => {
+      openModal(modalWindow, openClass, closingClass);
+      body.style.overflow = "hidden";
+    });
+  }
+  //closing button
+  closeBtn.addEventListener("click", () => {
+    body.style.overflow = "visible";
+    closeModal(modalWindow, openClass, closingClass);
+  });
+
+  if (arrElemClose) eventCloseArrElements(arrElemClose, modalWindow, openClass, closingClass);
+  if (arrElemOpen) eventOpenArrElements(arrElemOpen, modalWindow, openClass, closingClass);
 }
